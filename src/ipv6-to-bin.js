5ab68430be9f7cf4192f54bb8180fd3cc6607346
ozipi <ozipi.nomad@gmail.com>
2016-03-04 23:51:51 -0600

    initial src and test files

---
+++ src/ipv6-to-bin.js
@@ -0,0 +1,14 @@
+var hexToBinary = require('hex-to-binary');
+
+function ipv6ToBin(ip) {
+  var ipSplit = ip.split(":");
+  var result = [];
+  for (var i = 0; i < ipSplit.length; i++) {
+	  var binarySegment = hexToBinary(ipSplit[i]);
+	  result.push(binarySegment);
+  }
+
+  return result.join(":");
+}
+
+module.exports = ipv6ToBin;
